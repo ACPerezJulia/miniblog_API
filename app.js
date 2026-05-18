abcb451772bb1d7088d1af86d99dd33cbb0e9de9
ACPerezJulia <121969687+ACPerezJulia@users.noreply.github.com>
2026-05-17 21:18:49 -0300

    refactor(config): centralizada la carga de variables de entorno en server.js

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import yaml from "js-yaml";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
